8766f28de479ecd9aee608784b32c05533a786d8
Anastasia <nastazka8@gmail.com>
2024-04-01 18:38:59 +0300

    папка проекта

---
+++ interno/src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <header-component></header-component>
+      <div class="blog">
+        <p class="blog-title">Articles & News</p>
+        <p class="blog-subtitle">It is a long established fact that a reader will be distracted by the of readable content of a page when lookings at its layouts the points of using.</p>
+        <articles-component></articles-component>
+      </div>
+      <footer-component></footer-component>
+    </div>
+  </div>
+</template>
+
+<script>
+import ArticlesComponent from './components/ArticlesList.vue'
+import HeaderComponent from './components/HeaderComponent.vue'
+import FooterComponent from './components/FooterComponent.vue'
+
+export default {
+  name: 'App',
+  components: {
+    ArticlesComponent,
+    HeaderComponent,
+    FooterComponent
+  }
+}
+</script>
+
+<style>
+.container {
+    width: 1200px;
+    margin:  0 360px 0 360px;
+}
+.blog-title {
+    font-family: DM Serif Display;
+    font-size: 50px;
+    font-weight: 400;
+    line-height: 63px;
+    letter-spacing: 0.02em;
+    text-align: center;
+}
+.blog-subtitle {
+    font-family: Jost;
+    font-size: 22px;
+    font-weight: 400;
+    line-height: 33px;
+    letter-spacing: 0.01em;
+    text-align: center;
+    width: 811px;
+    margin-left: 195px;
+}
+</style>
---
+++ interno/src/components/ArticlesList.vue
@@ -0,0 +1,131 @@
+<template>
+    <div id="articles-news" class="blog-articles">
+        <div class="articles-list">
+            <article v-for="article in articlesData" :key="article.id">
+                <img :src="article.image" alt="Изображение новости">
+                <p class="article-title">{{ article.title }}</p>
+                <div class="article-footer">
+                    <span class="article-date">{{ article.date }}</span>
+                    <a class="article__link">
+                        <svg
+                            width="9"
+                            height="16"
+                            viewBox="0 0 9 16"
+                            fill="none"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path
+                            d="M1.77144 14.9527L7.71429 8.267L1.77144 1.58128"
+                            stroke="#292F36"
+                            stroke-width="2"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            />
+                        </svg>
+                    </a>
+                </div>
+            </article>
+        </div>
+    </div>
+</template>
+
+<script>
+
+const articlesData = [
+    {
+        id: 1,
+        title: "Let’s Get Solution For Building Construction Work",
+        image: "../../../img/Article1.png",
+        date: "26 December,2022",
+    },
+    {
+        id: 2,
+        title: "Low Cost Latest Invented Interior Designing Ideas.",
+        image: "../../../img/Article2.png",
+        date: "22 December,2022",
+    },
+    {
+        id: 3,
+        title: "Best For Any Office & Business Interior Solution",
+        image: "../../../img/Article3.png",
+        date: "25 December,2022",
+    },
+    {
+        id: 4,
+        title: "Let’s Get Solution For Building Construction Work",
+        image: "../../../img/Article4.png",
+        date: "26 December,2022",
+    },
+    {
+        id: 5,
+        title: "Low Cost Latest Invented Interior Designing Ideas.",
+        image: "../../../img/Article5.png",
+        date: "22 December,2022",
+    },
+    {
+        id: 6,
+        title: "Best For Any Office & Business Interior Solution",
+        image: "../../../img/Article6.png",
+        date: "25 December,2022",
+    },
+];
+
+export default {
+    data() {
+        return {
+            articlesData: articlesData,
+        };
+    },
+};
+</script>
+
+<style>
+.articles-list {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    column-gap: 21px;
+    row-gap: 30px;
+    margin-top: 52px;
+}
+article {
+    position: relative;
+    height: 521px;
+    padding: 21px;
+    border: 1px #e7e7e7 solid;
+    border-radius: 62px;
+}
+.article-title {
+    font-family: DM Serif Display;
+    font-size: 25px;
+    font-weight: 400;
+    line-height: 31px;
+    letter-spacing: 0.02em;
+    text-align: left;
+    width: 330px;
+}
+.article-footer {
+    display: flex;
+    justify-content: space-between;
+    margin-top: 30px;
+}
+.article-date {
+    font-family: Jost;
+    font-size: 16px;
+    font-weight: 400;
+    line-height: 24px;
+    letter-spacing: 0.01em;
+    text-align: left;
+}
+.article__link {
+    position: absolute;
+    bottom: 37px;
+    right: 21px;
+    width: 52px;
+    height: 52px;
+    line-height: 60px;
+    text-align: center;
+    border-radius: 50%;
+    background-color: #f4f0ec;
+    cursor: pointer;
+}
+</style>
